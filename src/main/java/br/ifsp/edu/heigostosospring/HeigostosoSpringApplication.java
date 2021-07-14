eb3419c2ef203ac3c8fdb5f510cd0a9f438250ef
Bleiner Mathias <bleinerbbm@hotmail.com>
2021-07-13 22:34:23 -0300

    Initial commit

---
+++ src/main/java/br/ifsp/edu/heigostosospring/HeigostosoSpringApplication.java
@@ -0,0 +1,22 @@
+package br.ifsp.edu.heigostosospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HeigostosoSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeigostosoSpringApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value="myName", defaultValue = "Antonio") String name){
+        return String.format("Hello %s", name);
+    }
+
+}
---
+++ src/test/java/br/ifsp/edu/heigostosospring/HeigostosoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ifsp.edu.heigostosospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeigostosoSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
