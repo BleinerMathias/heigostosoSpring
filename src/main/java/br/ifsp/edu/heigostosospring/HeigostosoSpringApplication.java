118f333754fbf9981d6c064a4c8cf7d12e692afc
Bleiner Mathias <bleinerbbm@hotmail.com>
2021-07-14 21:02:55 -0300

    Criando fragments header e footer

--- src/main/java/br/ifsp/edu/heigostosospring/HeigostosoSpringApplication.java
+++ src/main/java/br/ifsp/edu/heigostosospring/HeigostosoSpringApplication.java
@@ -3,6 +3,7 @@ package br.ifsp.edu.heigostosospring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 
 @SpringBootApplication
 @RestController
@@ -10,6 +11,10 @@ public class HeigostosoSpringApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HeigostosoSpringApplication.class, args);
+    }
+
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
     }
 
 }
